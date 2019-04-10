0ca6d605f0dda31772f085ef4908e183066ecaca
楊竣宇 <ch20026103@gmail.com>
2019-04-10 20:02:37 +0800

    no message

--- js/carcousel.js
+++ js/carcousel.js
@@ -1,32 +1,25 @@
-// Slow scrolling
-function slowScrolling() {
-  let anchors = document.querySelectorAll('a[href*="#"]')
-
-  for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault()
-
-      let blockID = anchor.getAttribute('href')
-
-      document.querySelector('' + blockID).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-    });
-  };
-};
-slowScrolling();
-
-// Init LGallery
-let x = initLG();
-function clickCard(){
-  let newGoods = document.getElementsByClassName('newGood');
-  for (let newGood in newGoods) {
-      newGoods[newGood].addEventListener('click',function(e){
-      console.log(e.currentTarget.querySelector('.imgCard'));
-      e.currentTarget.querySelector('.imgCard').classList.toggle("clickleaveCard")
-      e.currentTarget.querySelector('.imgCard').classList.toggle("clickCard");
-    })
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+  margin:30,
+  items: 1,
+  autoplay: true,
+  autoplayHoverPause: true,
+  nav:true,
+  autoplayTimeout: 2000,
+  center:true,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          items:5
+      },
+      1000:{
+          items:7
+      }
   }
-}
-clickCard();
+})
