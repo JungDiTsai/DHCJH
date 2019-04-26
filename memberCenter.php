411512097374b7b08f5fc31a369657cbb6c51b25
楊竣宇 <ch20026103@gmail.com>
2019-04-26 13:03:13 +0800

    20190426

---
+++ memberCenter.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>會員中心</title>
+    <link rel="stylesheet" href="css/memberCenter.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+    <!-- header -->
+    <input type="checkbox" id=menu_control>
+    <header>    
+        <!-- 放bar選單 -->
+        <label for="menu_control" class="menubtn" >
+                <div class="menuandcross"></div>
+        </label>
+        <nav class="mainNav">
+            <li class="mainNavCell"><a href="customized_01.html">電子花車客製</a></li>
+            <li class="mainNavCell"><a href="flyer.html">客製化宣傳單</a></li>
+            <h1 class="navLogo"><a href="index.html"><img src="images/logo.png" alt="台灣大舞台"></a></h1>
+            <li class="mainNavCell"><a href="beautyPageant.html">花車選美</a></li>
+            <li class="mainNavCell"><a href="intro.html">花車特色介紹</a></li>
+            <div class="navMemBtn">
+                <i class="far fa-user-circle"></i>
+            </div>
+        </nav>       
+    </header>
+    <div class="box"></div>
+    <!-- 並排 -->
+    <div class="wrap">
+        <!-- 會員中心側邊攔 -->
+        <div id="memberNav" class="col-md-2">
+            <div class="memberImg max_view">
+                <label for="memberImgFile">
+                    <img src="images/member2.jpg" alt="">
+                </label>
+            </div>
+            <h3 class="max_view">李三十四</h3>
+            <ul>
+                <li class="here"><a href="memberCenter.html">帳號管理</a></li>
+                <li><a href="memberOrder.html">訂單管理</a></li>
+                <li><a href="memberBeauty.html">我的發表</a></li>
+                <li><a href="memberCollection.html">我的收藏</a></li>
+                <li><a href="memberFlyer.html">宣傳單管理</a></li>
+                <li><a href="memberCoupons.html">我的優惠券</a></li>
+                <li><a href="index.html">登出</a></li>
+            </ul>
+        </div>
+        <!-- 會員中心帳號管理內容開始 -->
+        <div id="memberData" class="col-md-10">
+            <div class="memberImg min_view">
+                <label for="memberImgFile">
+                    <img src="images/member2.jpg" alt="">
+                </label>
+            </div>
+            <input type="file" name="memberImg" id="memberImgFile">
+            <h3 class="min_view">李三十四</h3>
+            <form action="">
+                <fieldset>
+                    <h3>帳號管理</h3>
+                    <table>
+                        <tr>
+                            <th>會員帳號</th>
+                            <td><input type="text" value="abcd1234"></td>
+                        </tr>
+                        <tr>
+                            <th>會員密碼</th>
+                            <td class="memPswInfo">**********<i class="fas fa-pen"></i></td>
+                        </tr>
+                        <tr class="changePSW" style="opacity: 0;position: absolute;">
+                            <td>舊密碼</td>
+                            <td><input type="text"></td>
+                        </tr>
+                        <tr class="changePSW" style="opacity: 0;position: absolute;">
+                            <td>新密碼</td>
+                            <td><input type="text"></td>
+                        </tr>
+                        <tr class="changePSW" style="opacity: 0;position: absolute;">
+                            <td>確認新密碼</td>
+                            <td><input type="text"></td>
+                        </tr>
+                        <tr>
+                            <th>會員信箱</th>
+                            <td><input type="email" value="abcd1234@gmail.com"></td>
+                        </tr>
+                        <tr>
+                            <th>會員電話</th>
+                            <td><input type="tel" value="0911111111"></td>
+                        </tr>
+                        <tr>
+                            <th>會員姓名</th>
+                            <td><input type="text" value="李三十四"></td>
+                        </tr>
+                        <tr>
+                            <th colspan="2"><input type="submit" value="確定修改" class="commonBtn"></th>
+                        </tr>
+                    </table>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script>
+        // 點擊會員密碼修改筆----------------
+        document.querySelector('.fa-pen').addEventListener('click', function () {
+            let changePSWs = document.querySelectorAll('.changePSW')
+            for (let key in changePSWs) {
+                let style = changePSWs[key].style.opacity;
+                if (style == "0") {
+                    changePSWs[key].style.opacity = '1';
+                    changePSWs[key].style.position = 'relative';
+                } else {
+                    changePSWs[key].style.opacity = '0';
+                    changePSWs[key].style.position = 'absolute';
+                }
+            }
+        })
+
+        
+        //螢幕寬度
+        let screenWidth = document.body.clientWidth;
+        if(screenWidth<=768){
+            
+        }
+    </script>
+</body>
+</html>
