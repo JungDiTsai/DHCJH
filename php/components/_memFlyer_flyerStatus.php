fb75c4c5ca8b9437aae52e2bb13b8bfc5b1f2649
楊竣宇 <ch20026103@gmail.com>
2019-04-29 18:52:22 +0800

    no message

---
+++ php/components/_memFlyer_flyerStatus.php
@@ -0,0 +1,21 @@
+<?php
+    $orderNo =  $_REQUEST['orderNo'];
+    $setting = $_REQUEST['setting'];
+
+    require_once("_connectDHC.php");
+
+    $errMsg='';
+    try {
+       $sql = "UPDATE flyer SET flyeStatus=$setting where orderNo = $orderNo";
+       $products = $pdo->exec($sql);
+       if($setting==1){
+            echo "修改成功 啟動";
+       }
+       else{
+            echo "修改成功 關閉";
+       }
+    } catch (PDOException $e) {
+       $errMsg .=  '錯誤原因' . $e->getMessage() . '<br>'; 
+       $errMsg .=  '錯誤行號' . $e->getLine() . '<br>';
+    }
+?>
