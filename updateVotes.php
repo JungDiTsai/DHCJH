662d9b55240a19b5bec55c572fa3623aa96e076b
jungdi <chung.di.cai@gmail.com>
2019-04-30 02:48:43 +0800

    no message

--- updateVotes.php
+++ updateVotes.php
@@ -1,9 +1,9 @@
 <?php
-    require_once("php/components/_connectDHC.php");
+    // require_once("php/components/_connectDHC.php");
     try{
         $dsn = "mysql:host=localhost;port=3306;dbname=dhc;charset=utf8";
-        $user = "cd106g1";
-        $password = "cd106g1";
+        $user = "Jung";
+        $password = "626425";
         $options = array(PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
         $pdo = new PDO($dsn, $user, $password, $options);
         $sql = "UPDATE orders set orderVote = orderVote + {$_REQUEST["amount"]} where orderNo = {$_REQUEST["orderNo"]}";
