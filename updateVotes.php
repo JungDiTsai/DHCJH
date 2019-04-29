a32ec01f9f3342c3e8395dc7008c6ea109a2ff64
Danny Hou <b00608052@gmail.com>
2019-04-30 02:35:32 +0800

    no message

--- updateVotes.php
+++ updateVotes.php
@@ -1,11 +1,7 @@
 <?php
-    require_once("php/components/_connectDHC.php");
+    
     try{
-        $dsn = "mysql:host=localhost;port=3306;dbname=dhc;charset=utf8";
-        $user = "cd106g1";
-        $password = "cd106g1";
-        $options = array(PDO::ATTR_CASE=>PDO::CASE_NATURAL, PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-        $pdo = new PDO($dsn, $user, $password, $options);
+        require_once("php/components/_connectDHC.php");
         $sql = "UPDATE orders set orderVote = orderVote + {$_REQUEST["amount"]} where orderNo = {$_REQUEST["orderNo"]}";
         //exit($sql);
         $updateVotes = $pdo->exec($sql);
