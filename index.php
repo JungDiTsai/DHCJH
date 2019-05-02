560859424145b843c82e5dc6fa07219c3d1311ac
Danny Hou <b00608052@gmail.com>
2019-05-02 13:08:30 +0800

    no message

--- index.php
+++ index.php
@@ -71,11 +71,11 @@ try{
     <script> 
 
         window.addEventListener("load",function(){
-            var storage = window.sessionStorage;
-            if( storage["alreadyShow"] == undefined){
-                storage["alreadyShow"] = "yes";   
-                var loadFadeOut = function(){
-                console.log('hi');
+            // var storage = window.sessionStorage;
+            // if( storage["alreadyShow"] == undefined){
+            //     storage["alreadyShow"] = "yes";   
+                 var loadFadeOut = function(){
+                 console.log('hi');
                 $(".loading").fadeOut(1800);
 
                 $("#introCarStageOne").css("left","0");
@@ -86,14 +86,14 @@ try{
             $("#car").removeClass('readyLoadingCar');
             $("#car").addClass('loadingCar');
             setTimeout(loadFadeOut,5200);   
-            }else{ //已經載入過了
-                $("#loadingScene").fadeOut(300);
-                $("#introCarStageOne").css("left","0");
-                $("#introCarStageOne").fadeIn(600);
-                $("#introWords").css("transform","scale(1)"); 
-                console.log('hi'); 
-                //setTimeout(loadFadeOut,1000);  
-            }
+            // }else{ //已經載入過了
+            //     $("#loadingScene").fadeOut(300);
+            //     $("#introCarStageOne").css("left","0");
+            //     $("#introCarStageOne").fadeIn(600);
+            //     $("#introWords").css("transform","scale(1)"); 
+            //     console.log('hi'); 
+            //     //setTimeout(loadFadeOut,1000);  
+            // }
       
         });
          
